13d515e2d146426378715b098f2458229180fb79
Yevhenii Hovykovych <userks1303j2@gmail.com>
2026-09-12 22:35:26 +0300

    Add the logic and UI of entering students
    Changes:
    * Update UI
    * Adjust business logic
    
    Logic:
    * Adding students
    * Entering their atributes (such as marks)

--- Learning_Git_01/MyForm.cpp
+++ Learning_Git_01/MyForm.cpp
@@ -2,3 +2,34 @@
 
 #include "MyForm.h"
 
+System::Void LearningGit01::MyForm::button1_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	try
+	{
+		String^ studentName = textBox1->Text;
+		if (String::IsNullOrWhiteSpace(studentName))
+		{
+			MessageBox::Show("Please enter a valid student name.", "Input", MessageBoxButtons::OK, MessageBoxIcon::Warning);
+			return;
+		}
+		// Add the student to the list
+	}
+	catch (Exception^ ex)
+	{
+		MessageBox::Show("An error occurred: " + ex->Message, "Error", MessageBoxButtons::OK, MessageBoxIcon::Error);
+	}
+}
+
+System::Void LearningGit01::MyForm::button2_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	try
+	{
+		int mark = Int32::Parse(textBox2->Text);
+		// Add the mark to the current student
+	}
+	catch (FormatException^ ex)
+	{
+		MessageBox::Show("Enter a number");
+		MessageBox::Show("Enter something meaningful", "My own tip", MessageBoxButtons::YesNoCancel, MessageBoxIcon::Hand);
+	}
+}
